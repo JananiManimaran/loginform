507498e6009cdf9fb7ca287d57cece9c73ee9e81
JananiManimaran <manij5055@gmail.com>
2024-01-23 04:58:47 -0800

    loginform

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Loginform from './Loginform'
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function App() {
+  return (
+    <>
+     <Loginform/>
+    </>
+  )
+}
+
